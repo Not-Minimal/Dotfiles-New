bafbd09dcbd94e486da2b9c22b2cbd5ebe912943
Not-Minimal <sam.pedreros14@gmail.com>
2025-10-30 15:10:22 -0300

    Se corrige warning lsp doble en config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -23,6 +23,19 @@ return {
             inlay_hints = { enabled = false },
             ---@type lspconfig.options
             servers = {
+                ["*"] = {
+                    keys = {
+                        {
+                            "gd",
+                            function()
+                                -- DO NOT RESUSE WINDOW
+                                require("telescope.builtin").lsp_definitions({ reuse_win = false })
+                            end,
+                            desc = "Goto Definition",
+                            has = "definition",
+                        },
+                    },
+                },
                 cssls = {},
                 tailwindcss = {
                     root_dir = function(...)
@@ -144,22 +157,5 @@ return {
             },
             setup = {},
         },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        opts = function()
-            local keys = require("lazyvim.plugins.lsp.keymaps").get()
-            vim.list_extend(keys, {
-                {
-                    "gd",
-                    function()
-                        -- DO NOT RESUSE WINDOW
-                        require("telescope.builtin").lsp_definitions({ reuse_win = false })
-                    end,
-                    desc = "Goto Definition",
-                    has = "definition",
-                },
-            })
-        end,
     },
 }
